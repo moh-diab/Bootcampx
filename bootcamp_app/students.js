f896932db9015df0c7ffb48ecca752ec59962b2f
moh-diab <mohdiab@gmail.com>
2022-01-06 20:20:43 -0500

    sql injection demo & add safeguard feature

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,15 +8,33 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString =`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${args[0]}%'
-LIMIT ${args[1] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const values = [`%${args[0]}%`, `${args[1] || 5}`]; 
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
 });
+
+
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${args[0]}%'
+// LIMIT ${args[1] || 5};
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   })
+// });
